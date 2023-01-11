7c5b60b9b7d095098b04569d4d98a2eafe80ed24
Nicobusta <108198942+Nicobusta@users.noreply.github.com>
2023-01-11 19:00:13 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,14 @@
+document.getElementById('luna').style.display="none";
+
+document.getElementById('sol').addEventListener("click", function( ){
+    document.documentElement.setAttribute('tema', 'dark');
+    document.getElementById('sol').style.display="none";
+    document.getElementById('luna').style.display="block"; 
+});
+
+document.getElementById('luna').addEventListener("click", function( ){
+    document.documentElement.setAttribute('tema', 'light');
+    document.getElementById('luna').style.display="none";
+    document.getElementById('sol').style.display="block"; 
+});         
+        
