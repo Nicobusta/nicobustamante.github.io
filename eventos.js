a02ae906710f6af6855c0414db1b927d0165ac48
Nicolas Bustamante <nicobusta38@gmail.com>
2023-01-11 15:37:35 -0300

    modificacion selector

--- eventos.js
+++ eventos.js
@@ -1,6 +1,4 @@
-/* const colorSwitch = document.querySelector('#switch input[type="checkbox"]'); */
 document.getElementById('luna').style.display="none";
-
 
 document.getElementById('sol').addEventListener("click", function( ){
     document.documentElement.setAttribute('tema', 'dark');
