e459f88397fc6467e9bac7e96b0eb0b728c7a8f2
Nicolas Bustamante <nicobusta38@gmail.com>
2023-01-11 15:27:28 -0300

    selector modo claro y oscuro

--- eventos.js
+++ eventos.js
@@ -1,10 +1,16 @@
-const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
-            function cambiaTema(ev){
-                if(ev.target.checked){
-                    document.documentElement.setAttribute('tema', 'dark');
-                } else {
-                    document.documentElement.setAttribute('tema', 'light');
-                }
-            }
-            colorSwitch.addEventListener('change', cambiaTema);
+/* const colorSwitch = document.querySelector('#switch input[type="checkbox"]'); */
+document.getElementById('luna').style.display="none";
 
+
+document.getElementById('sol').addEventListener("click", function( ){
+    document.documentElement.setAttribute('tema', 'dark');
+    document.getElementById('sol').style.display="none";
+    document.getElementById('luna').style.display="block"; 
+});
+
+document.getElementById('luna').addEventListener("click", function( ){
+    document.documentElement.setAttribute('tema', 'light');
+    document.getElementById('luna').style.display="none";
+    document.getElementById('sol').style.display="block"; 
+});         
+        
